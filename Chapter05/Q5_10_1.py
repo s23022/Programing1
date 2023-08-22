ca61f6ebd1da136189102649c5f8764ee5df3988
s23022 <s23022@std.it-college.ac.jp>
2023-08-22 07:59:38 +0000

    p277 課題

---
+++ Chapter05/Q5_10_1.py
@@ -0,0 +1,9 @@
+def is_number_of_positions_of_10(x):
+    return 10 <= x < 20
+
+
+# テスト
+print(is_number_of_positions_of_10(5))  # False
+print(is_number_of_positions_of_10(15))  # True
+print(is_number_of_positions_of_10(20))  # False
+print(is_number_of_positions_of_10(10))  # True
